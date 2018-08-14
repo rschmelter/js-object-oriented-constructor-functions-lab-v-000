e008d299fff372b8f73f386ec5f236e4910ca222
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-14 23:43:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,12 @@ function Driver(name, age, experience) {
   this.name = name;
   this.age = age;
   this.experience = experience;
+
+}
+
+function PickupLocation (address, city) {
+  this.address = address;
+  this.city = city;
+
   
 }
